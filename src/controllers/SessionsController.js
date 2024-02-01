677cf022de7afe29107ef2f215076f4c24c905b9
Marcos Colella <colella.dev@gmail.com>
2024-02-01 10:59:39 -0300

    Validação Usuário e Senha

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,26 @@
+const knex = require("../database/knex")
+const AppError = require("../AppError")
+const { compare } = require("bcryptjs");
+
+
 class SessionsController {
     async create(request, response) {
         const { email, password } = request.body
 
-            return response.json({ email, password})
+        const user = await knex("users").where({ email }).first()
+        
+        if(!user) {
+            throw new AppError("E-mail e/ou senha incorreta", 401) // Verifica se o User existe
+        }
+
+        const passwordMatched = await compare(password, user.password); //Compara as senhas
+
+		    if (!passwordMatched) {
+			      throw new AppError("E-mail e/ou senha incorreta", 401); //Verifica se a senha bate
+		    }
+
+        return response.json({ user })
+
       }
     }
 

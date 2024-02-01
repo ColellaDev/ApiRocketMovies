ff3dbcb94dc6c5ffb6d6f76907464ee215c02ff5
Marcos Colella <colella.dev@gmail.com>
2024-02-01 15:02:10 -0300

    Salvando imagem no Banco de Dados

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,21 +4,20 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 
-
 const UsersController = require("../controllers/UsersController") // Importando a Classe
+const UserAvatarController  = require("../controllers/UserAvatarController")
 
 const usersController = new UsersController() // Instanciando a Classe
 
+
+const userAvatarController = new UserAvatarController()
 const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
 usersRoutes.delete("/id", usersController.delete)
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-    console.log(request.file.filename)
-    response.json()
-})
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
 
